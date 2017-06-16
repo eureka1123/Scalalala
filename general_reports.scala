37555b986582a65c1e8d9de68dcad10f002c6e8d
eureka1123 <xl.elaine.guo@gmail.com>
2017-06-16 10:34:19 -0400

    update on global variables

--- general_reports.scala
+++ general_reports.scala
@@ -2,11 +2,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-val TOPIC = "missmalini"
-val TOPIC_NAME = "MissMalini"
-val REPORT_DIR = "/home/xiaoluguo/missmalini/"
-val DATA_ROOT = "hdfs://10.142.0.63:9000/data"
-val ARCHIVE_ROOT = "hdfs://10.142.0.63:9000/" + TOPIC
-val ENTITY_FILE = TOPIC + "_entities2.txt"
-val SLICES = 2000
+object GeneralReports{
+	global val TOPIC = "missmalini"
+	global val TOPIC_NAME = "MissMalini"
+	global val REPORT_DIR = "/home/xiaoluguo/missmalini/"
+	global val DATA_ROOT = "hdfs://10.142.0.63:9000/data"
+	global val ARCHIVE_ROOT = "hdfs://10.142.0.63:9000/" + TOPIC
+	global val ENTITY_FILE = TOPIC + "_entities2.txt"
+	global val SLICES = 2000
+
+}
+
+
 
