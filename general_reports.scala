39ad538de146fee3fd5ab43c26af11f9f90d3a92
Abishkar <abishkar@mit.edu>
2017-06-16 12:23:49 -0400

    Cleaned it up

--- general_reports.scala
+++ general_reports.scala
@@ -3,45 +3,41 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object GeneralReports{
-	val TOPIC = "missmalini"
-	val TOPIC_NAME = "MissMalini"
-	val REPORT_DIR = "/home/xiaoluguo/missmalini/"
-	val DATA_ROOT = "hdfs://10.142.0.63:9000/data"
-	val ARCHIVE_ROOT = "hdfs://10.142.0.63:9000/" + TOPIC
-	val ENTITY_FILE = TOPIC + "_entities2.txt"
-	val SLICES = 2000
-	
-	//def safe_date_from_str(input:String) : String = {
-	//}
-	
-	//def safe_float(input:String) : Float ={
-	//}
+    val TOPIC = "missmalini"
+    val TOPIC_NAME = "MissMalini"
+    val REPORT_DIR = "/home/xiaoluguo/missmalini/"
+    val DATA_ROOT = "hdfs://10.142.0.63:9000/data"
+    val ARCHIVE_ROOT = "hdfs://10.142.0.63:9000/" + TOPIC
+    val ENTITY_FILE = TOPIC + "_entities2.txt"
+    val SLICES = 2000
 
-	def main(args: Array[String]) {
-	
-	
+    
+    //def safe_date_from_str(input:String) : String = {
+    //}
+    
+    //def safe_float(input:String) : Float ={
+    //}
 
-    val likeFacts = sc.textFile(DATA_PATH, SLICES)
-        .map(x => x.split("/t"))
-        .filter(x => (x.length > 2))
-        .map(x => (x(0),x(1),x(2)))
-        .distinct()
+    def main(args: Array[String]) {
 
-    val dimLikes = sc.textFile(DATA_PATH, SLICES)
-        .map(x => x.split("/t"))
-        .filter(x => (x.length > 3))
-        .setName("dimLikes")
-        .cache()
+        val likeFacts = sc.textFile(DATA_ROOT+"/fact_like", SLICES)
+            .map(x => x.split("/t"))
+            .filter(x => (x.length > 2))
+            .map(x => (x(0),x(1),x(2)))
+            .distinct()
 
-    val iaFbMapB = sc.broadcast((dim_likes.map(lambda x: (x[0] -> x[3])).distinct().collect()).toMap)
+        val dimLikes = sc.textFile(DATA_ROOT+"/dim_like", SLICES)
+            .map(x => x.split("/t"))
+            .filter(x => (x.length > 3))
+            .setName("dimLikes")
+            .cache()
 
-    val likes = dimLikes
-        .map(x => (x(0) -> (x(1) -> x(2))))
-        .distinct()
+        val iaFbMapB = sc.broadcast(dimLikes.map(x => (x(0), x(3))).distinct().collect().toMap)
 
-}
+        val likes = dimLikes.map(x => (x(0), (x(1), x(2))))
+            .distinct()
 
-
+    }
 }
 
 
